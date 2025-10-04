ef5d6f2f6c670914b49222f1671566c8df2b9a46
Your Name <your-email@example.com>
2025-10-04 14:11:54 +0700

    fix: update package.json & remove old postcss.config.cjs

--- client/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-// file: client/postcss.config.js
-
-module.exports = {
-  plugins: {
-    // Chỉ định rõ ràng các plugin (sẽ được tìm thấy trong node_modules/ của client)
-    'tailwindcss': {},
-    'autoprefixer': {},
-  },
-};
