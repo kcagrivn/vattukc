1f5b80c7330d042251f8c69a05643f4ae6410cfb
Your Name <your-email@example.com>
2025-10-04 13:20:29 +0700

    fix: final tailwind build configuration for netlify

---
+++ client/postcss.config.cjs
@@ -0,0 +1,9 @@
+// file: client/postcss.config.js
+
+module.exports = {
+  plugins: {
+    // Chỉ định rõ ràng các plugin (sẽ được tìm thấy trong node_modules/ của client)
+    'tailwindcss': {},
+    'autoprefixer': {},
+  },
+};
