3753071c2ad7910831b01ea71349f7ae5767fdc0
root <root@vps.vattukc.com>
2025-10-05 22:23:05 +0700

    🧱 Khai báo props cho OrderRow để fix lỗi TypeScript

--- src/components/admin/OrderRow.tsx
+++ src/components/admin/OrderRow.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
-export default function OrderRow() {
+interface AdminOrder {
+  _id: string
+  customerName: string
+  totalAmount: number
+  status: string
+  // 👉 Thêm các trường bạn đang dùng thực tế ở đây
+}
+
+interface OrderRowProps {
+  order: AdminOrder
+}
+
+export default function OrderRow({ order }: OrderRowProps) {
   return (
-    <div>
-      <p>OrderRow component</p>
-    </div>
+    <tr>
+      <td>{order.customerName}</td>
+      <td>{order.totalAmount}</td>
+      <td>{order.status}</td>
+    </tr>
   )
 }
 
